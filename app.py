dd8d13723af63d78cd620e0d6bbe1416e3d462eb
shaikgps93-ship-it <shaikgps93@gmail.com>
2026-06-18 20:16:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,20 @@ selected = option_menu(
             "background-color": "#E50914"
         }
     }
+    import streamlit as st
+
+if selected == "Dashboard":
+    st.switch_page("pages/Dashboard.py")
+
+elif selected == "Jobs":
+    st.switch_page("pages/Search_Jobs.py")
+
+elif selected == "Salary":
+    st.switch_page("pages/Salary_Predictor.py")
+
+elif selected == "Resume":
+    st.switch_page("pages/_Resume_Analyzer.py")
+
+elif selected == "AI Advisor":
+    st.switch_page("pages/_AI_Career_Advisor.py")
 )
